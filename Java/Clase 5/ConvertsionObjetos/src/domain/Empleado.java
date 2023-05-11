80570765efefa894b586dfb92a7c4b73264f4ab9
Emmanuel <pc-500@live.com.ar>
2023-05-10 21:12:35 -0300

    .

---
+++ Java/Clase 5/ConvertsionObjetos/src/domain/Empleado.java
@@ -0,0 +1,27 @@
+package domain;
+public class Empleado {
+    protected String nombre;
+    protected double sueldo;
+    
+public Empleado(String nombre, double sueldo){
+this.nombre=nombre;
+this.sueldo=sueldo;
+}
+public String obtenerDetalle(){
+return"Nombre: "+this.nombre+", Sueldo: "+this.sueldo;
+}
+    public String getNombrte() {
+        return nombre;
+    }
+    public void setNombre(String nombre){
+    this.nombre=nombre;
+    }
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+}
