b5884b072ecb1fe039ec2224b829166e84ad2ade
Emmanuel <pc-500@live.com.ar>
2023-06-05 22:48:30 -0300

    .

--- Python/Clase8/PersonaDao.py
+++
@@ -1,9 +0,0 @@
-class PersonaDao:
-    """
-    DAO=data access object
-    CRUD= create,read,update,delete
-    """
-    _seleccionar = 'SELECT * FROM persona ORDER BY id_persona'
-    _insertar = 'INSERT INTO persona(,nombre,apellido,email)VALUES(,%s,%s,%s)'
-    _actualizar = 'UPDATE persona SET nombre = %s,apellido,email=%s,email=%s WHERE id_persona=%s'
-    _eliminar = 'DELETE FROM persona WHERE id_persona= %s'
