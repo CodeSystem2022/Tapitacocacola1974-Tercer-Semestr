53907df920f4b62e3582f7a6c480ca2792170973
Joaquin Avaro Acosta <95146374+joakko18@users.noreply.github.com>
2023-06-26 00:31:12 +0300

    Update app.py
    .

--- FinalProject/PYTHON-PROJECT/app.py
+++ FinalProject/PYTHON-PROJECT/app.py
@@ -34,93 +34,110 @@ def get_connection():
 
 @app.get('/api/users')
 def get_users():
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.execute('SELECT * FROM users')
-    users = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(users)
+        cur.execute('SELECT * FROM users')
+        users = cur.fetchall()
+        cur.close()
+        conn.close()
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while retrieving users: ' + str(e)}), 500
 
 
 @app.post('/api/users')
 def create_user():
+    try:
 
-    new_user = request.get_json()
-    username = new_user['username']
-    email = new_user['email']
-    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
-    print(username, email, password)
 
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+        new_user = request.get_json()
+        username = new_user['username']
+        email = new_user['email']
+        password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+        print(username, email, password)
 
-    cur.execute('INSERT INTO users (username,email,password)VALUES(%s,%s,%s)RETURNING *',
-                (username, email, password))
-    new_created_user = cur.fetchone()
-    print(new_created_user)
-    conn.commit()
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.close()
-    conn.close()
-    return jsonify(new_created_user)
+        cur.execute('INSERT INTO users (username,email,password)VALUES(%s,%s,%s)RETURNING *',
+                    (username, email, password))
+        new_created_user = cur.fetchone()
+        print(new_created_user)
+        conn.commit()
+
+        cur.close()
+        conn.close()
+        return jsonify(new_created_user)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while creating the user: ' + str(e)}), 500
+        
 
 
 @app.delete('/api/users/<id>')
 def delete_users(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.execute('DELETE FROM users WHERE id = %s RETURNING *', (id, ))
-    user = cur.fetchone()
-    conn.commit()
+        cur.execute('DELETE FROM users WHERE id = %s RETURNING *', (id, ))
+        user = cur.fetchone()
+        conn.commit()
 
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
 
-    if user is None:
-        return jsonify({'message': 'User not delete'}), 404
+        if user is None:
+            return jsonify({'message': 'User not delete'}), 404
 
-    return jsonify(user)
+        return jsonify(user)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while deleting the user: ' + str(e)}), 500
 
 
 @app.put('/api/users/<id>')
 def update_users(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    new_user = request.get_json()
-    username = new_user['username']
-    email = new_user['email']
-    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
-    print(username, email, password)
+        new_user = request.get_json()
+        username = new_user['username']
+        email = new_user['email']
+        password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
+        print(username, email, password)
 
-    cur.execute(
-        'UPDATE users SET username=%s,email=%s,password=%s WHERE id=%s  RETURNING *', (username, email, password, id))
-    updated_user = cur.fetchone()
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.execute(
+            'UPDATE users SET username=%s,email=%s,password=%s WHERE id=%s  RETURNING *', (username, email, password, id))
+        updated_user = cur.fetchone()
+        conn.commit()
+        cur.close()
+        conn.close()
 
-    if updated_user is None:
-        return jsonify({'message': 'User not found'}), 404
+        if updated_user is None:
+            return jsonify({'message': 'User not found'}), 404
 
-    return jsonify(updated_user)
+        return jsonify(updated_user)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while updating the user: ' + str(e)}), 500
 
 
 @app.get('/api/users/<id>')
 def get_user(id):
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=extras.RealDictCursor)
+    try:
+        conn = get_connection()
+        cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.execute('SELECT * FROM users WHERE id = %s', (id,))
-    user = cur.fetchone()
+        cur.execute('SELECT * FROM users WHERE id = %s', (id,))
+        user = cur.fetchone()
 
-    if user is None:
-        return jsonify({'message': 'User not found'}), 404
+        if user is None:
+            return jsonify({'message': 'User not found'}), 404
 
-    return jsonify(user)
+        return jsonify(user)
+    except Exception as e:
+        return jsonify({'message': 'An error occurred while retrieving the user: ' + str(e)}), 500
 
 @app.route('/')
 def home():
