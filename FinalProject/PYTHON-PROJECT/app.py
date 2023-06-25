3ab01113385fb960d63c0752d77d9800e159f2c7
Emmanuel <pc-500@live.com.ar>
2023-06-25 18:27:34 -0300

    Cambios
    cambios

--- FinalProject/PYTHON-PROJECT/app.py
+++ FinalProject/PYTHON-PROJECT/app.py
@@ -1,36 +1,23 @@
-from flask import Flask, request, jsonify,send_file,json
+from flask import Flask, request, jsonify, send_file
 from psycopg2 import connect, extras
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
-from os import environ
+import os
 
-load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 key = Fernet.generate_key()
 
-host =environ.get('DB_HOST')
-port = environ.get('DB_PORT')
-dbname =environ.get('DB_NAME')
-user = environ.get('DB_USER')
-password = environ.get('DB_PASSWORD')
-#host='localhost'
-#port=5432
-#dbname='usersdb'
-#user='postgres'
-#password="matiitas"
-#host='containers-us-west-209.railway.app'
-#port=7220
-#dbname='usersdb'
-#user='postgres'
-#password="rZ1erL8rIiv8juGGtRFU"
-
+host = os.environ.get('DB_HOST')
+port = os.environ.get('DB_PORT')
+dbname = os.environ.get('DB_NAME')
+user = os.environ.get('DB_USER')
+password = os.environ.get('DB_PASSWORD')
 
 def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname,
-                   user=user, password=password)
+    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
     return conn
-
 
 @app.get('/api/users')
 def get_users():
@@ -43,21 +30,19 @@ def get_users():
     conn.close()
     return jsonify(users)
 
-
 @app.post('/api/users')
 def create_user():
-
     new_user = request.get_json()
     username = new_user['username']
     email = new_user['email']
-    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
-    print(username, email, password)
+    direccion = new_user['direccion']
+    print(username, email, direccion)
 
     conn = get_connection()
     cur = conn.cursor(cursor_factory=extras.RealDictCursor)
 
-    cur.execute('INSERT INTO users (username,email,password)VALUES(%s,%s,%s)RETURNING *',
-                (username, email, password))
+    cur.execute('INSERT INTO users (username, email, direccion) VALUES (%s, %s, %s) RETURNING *',
+                (username, email, direccion))
     new_created_user = cur.fetchone()
     print(new_created_user)
     conn.commit()
@@ -65,7 +50,6 @@ def create_user():
     cur.close()
     conn.close()
     return jsonify(new_created_user)
-
 
 @app.delete('/api/users/<id>')
 def delete_users(id):
@@ -84,7 +68,6 @@ def delete_users(id):
 
     return jsonify(user)
 
-
 @app.put('/api/users/<id>')
 def update_users(id):
     conn = get_connection()
@@ -93,11 +76,12 @@ def update_users(id):
     new_user = request.get_json()
     username = new_user['username']
     email = new_user['email']
-    password = Fernet(key).encrypt(bytes(new_user['password'], 'utf-8'))
-    print(username, email, password)
+    direccion = new_user['direccion']
+    print(username, email, direccion)
 
     cur.execute(
-        'UPDATE users SET username=%s,email=%s,password=%s WHERE id=%s  RETURNING *', (username, email, password, id))
+        'UPDATE users SET username=%s, email=%s, direccion=%s WHERE id=%s RETURNING *',
+        (username, email, direccion, id))
     updated_user = cur.fetchone()
     conn.commit()
     cur.close()
@@ -107,7 +91,6 @@ def update_users(id):
         return jsonify({'message': 'User not found'}), 404
 
     return jsonify(updated_user)
-
 
 @app.get('/api/users/<id>')
 def get_user(id):
@@ -128,4 +111,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    #final
