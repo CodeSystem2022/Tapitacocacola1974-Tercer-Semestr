1f220e1d2ebee8b5d0e6df63ebfe42e4c6d3f16e
Joaquin Avaro Acosta <95146374+joakko18@users.noreply.github.com>
2023-06-28 05:29:16 +0300

    .
    .

--- FinalProject/PYTHON-PROJECT/static/main.js
+++ FinalProject/PYTHON-PROJECT/static/main.js
@@ -1,4 +1,4 @@
-const userForm = document.querySelector('#userForm');
+const userForm = document.querySelector('#userForm'); //iniciamos manipulando el dom 
 let users = [];
 let editing = false;
 let userId = null;
@@ -57,7 +57,7 @@ userForm.addEventListener('submit', async (e) => {
   userForm.reset();
 });
 
-function renderUser(users) {
+function renderUser(users) { //con esta funcion lo que hacemos es renderizar la informacion obtenida desde el backend o recibida desde el front end
   const userList = document.querySelector('#userList');
   console.log(userList);
   userList.innerHTML = '';
@@ -81,7 +81,7 @@ function renderUser(users) {
       <p>${user.direccion}</p>
     `;
 
-    const btnDelete = userItem.querySelector('.btn-delete');
+    const btnDelete = userItem.querySelector('.btn-delete'); //en este boton delete hemos creado una funcion que al clickear eliminael dato deseado
     btnDelete.addEventListener('click', async () => {
       const response = await fetch(`/api/users/${user.id}`, {
         method: 'DELETE',
@@ -91,7 +91,7 @@ function renderUser(users) {
       renderUser(users);
     });
 
-    const btnEdit = userItem.querySelector('.btn-edit');
+    const btnEdit = userItem.querySelector('.btn-edit'); // hemos creado una funcion en la cual el boton edit nos permite editar lo deseado
     btnEdit.addEventListener('click', async () => {
       const response = await fetch(`/api/users/${user.id}`);
       const data = await response.json();
